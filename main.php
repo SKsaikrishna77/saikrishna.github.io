1cfb4f2c7485c99cc034e43e170f73670e819414
saikrishna <50363831+SKsaikrishna77@users.noreply.github.com>
2025-05-28 21:07:17 +0530

    Update main.php

--- main.php
+++ main.php
@@ -23,6 +23,7 @@
 
 
 <?php
+session_start();
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $url = 'https://script.google.com/macros/s/AKfycbwoxmAext_TVbR6JTT9rPmy6sGp4V2woFEh4V9gBANNUjzhIVrOkipPdAMqFWynV2Zc/exec';
     $data = [
@@ -44,9 +45,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $result = file_get_contents($url, false, $context);
 
     if ($result === FALSE) {
-        echo "Error submitting form";
+        // echo "Error submitting form";
+        $_SESSION['status']="Not Submitted";
+        header('location: index.php');
     } else {
-        echo "Form submitted successfully!";
-    }
+        // echo "Form submitted successfully!";
+        $_SESSION['status']="submitted successfully!";
+        header('location: index.php');
+    }
 }
 ?>
